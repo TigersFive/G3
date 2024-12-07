23a94f216c9483fc121fef7f98480a5ccefedec1
vu2005 <vunnth2307024@fpt.edu.vn>
2024-12-08 00:24:00 +0700

    build không lỗi nhưng không chạy được dự án =))

--- CarInsuranceManage/obj/Debug/net8.0/CarInsuranceManage.AssemblyInfo.cs
+++ CarInsuranceManage/obj/Debug/net8.0/CarInsuranceManage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarInsuranceManage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5f88701ab6d3cd6f6300593545ce2f4a60d075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2254d2e14af2a4c4dd9d54e000276ab6d3dfb198")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarInsuranceManage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarInsuranceManage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
