f4a5d9e0ebb4a6e454efd3423d28cade41ffbc5e
doxuantruong120605 <truongdxth2307038@fpt.edu.vn>
2024-12-07 23:10:42 +0700

    Up Date Form Nhap

--- CarInsuranceManage/obj/Debug/net8.0/CarInsuranceManage.AssemblyInfo.cs
+++ CarInsuranceManage/obj/Debug/net8.0/CarInsuranceManage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarInsuranceManage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074395a82b3ef3c9f905f13dba51174226040c8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529296c67ce90e8c250e630dbe96b4a0afa65b37")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarInsuranceManage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarInsuranceManage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
